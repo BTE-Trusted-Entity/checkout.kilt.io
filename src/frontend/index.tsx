f4ed6d09ec96d686c00c6d26d04e3f96463e40f8
Marton Csernai <marton@kilt.io>
2022-11-07 14:13:34 +0100

    clean up based on tsc test

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -1,5 +1,4 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
 
@@ -10,11 +9,7 @@ function renderApp() {
   }
 
   const root = createRoot(container);
-  root.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  );
+  root.render(<App />);
 }
 
 renderApp();
