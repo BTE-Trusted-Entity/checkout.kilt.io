fcd79dfaa15999d189b407ac17f81b01a2e34ad6
Luke Grunau <lukegrunau@Lukes-MBP.fritz.box>
2022-11-09 12:36:15 +0100

    SK-1491: Basic auth

--- src/backend/utilities/configuration.ts
+++ src/backend/utilities/configuration.ts
@@ -6,9 +6,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 const { env } = process;
 
+const httpAuthPassword = env.SECRET_HTTP_AUTH_PASSWORD;
+
 export const configuration = {
   baseUri: env.BASE_URI || 'http://localhost:3000',
   port: env.PORT || 3000,
   isProduction: env.NODE_ENV === 'production',
   distFolder: path.join(cwd(), 'dist', 'frontend'),
+  httpAuthPassword,
 };
