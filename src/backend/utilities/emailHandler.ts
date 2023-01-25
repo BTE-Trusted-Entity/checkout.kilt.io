d1e3c3d29f45dc199b4fbeb45210d174dd38d782
Adel Golghalyani <ad96el@gmail.com>
2023-01-25 12:51:09 +0100

    refactor email handler

--- src/backend/utilities/emailHandler.ts
+++ src/backend/utilities/emailHandler.ts
@@ -1,4 +1,5 @@
-import * as path from 'path';
+import { cwd } from 'node:process';
+import path from 'node:path';
 
 import AWS from 'aws-sdk';
 import nodemailer from 'nodemailer';
@@ -9,45 +10,49 @@ const mailer = nodemailer.createTransport({
   SES: new AWS.SES(configuration.aws),
 });
 
-const sender = 'Kilt Checkout <checkout@kilt.io>';
+const sender = 'KILT Checkout <checkout@kilt.io>';
 const subject = 'Thanks for using the Checkout Service to get your KILT DID';
 
-function getContent(name: string, didCost: string) {
-  return `Dear ${name},
+export async function sendConfirmationEmail(recipient: string, name: string) {
+  const { didCost } = configuration;
+
+  const content = `Dear ${name},
+
 Thank you for using the Checkout Service for anchoring your DID on the 
 KILT blockchain for which we charged you ${didCost} Euro (including VAT) 
 through PayPal. Attached Terms and Conditions are applicable for your 
 order.
-  
+    
 Best regards,
-  
+    
 The team of the B.T.E.
-  
+    
 B.T.E. BOTLabs Trusted Entity GmbH
 Keithstraße 2-4
 10787 Berlin, Germany
 German Commercial Court:
 Amtsgericht Charlottenburg in Berlin
 Registration Number: HRB 231219B
-  
+    
 VAT No: DE 346528612
 Managing Director: Ingo Rübe
 Contact: info@botlabs.org
 Or go to Tech support and click on "Contact Us"
 Requirements according to § 5 TMG (Germany)`;
-}
 
-export async function sendConfirmationEmail(recipient: string, name: string) {
-  const { didCost } = configuration;
   await mailer.sendMail({
     from: sender,
     to: recipient,
-    subject: subject,
-    text: getContent(name, didCost),
+    subject,
+    text: content,
     attachments: [
       {
-        path: path.resolve(
-          './src/backend/resources/Terms_and_Conditions_Checkout_Service.pdf',
+        path: path.join(
+          cwd(),
+          'src',
+          'backend',
+          'resources',
+          'Terms_and_Conditions_Checkout_Service.pdf',
         ),
       },
     ],
