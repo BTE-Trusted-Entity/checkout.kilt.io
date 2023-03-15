a1cb5a9da098bf0bdaf35a0c2b0f6b8149b6b11e
Luke Grunau <lukegrunau@gmail.com>
2023-03-15 18:14:17 +0100

    Fix unknown error text (#174)

--- src/frontend/components/Transaction/TransactionTemplate.tsx
+++ src/frontend/components/Transaction/TransactionTemplate.tsx
@@ -302,8 +302,9 @@ export function TransactionTemplate({
                 It’s possible that the transaction was still successful. Please
                 wait a moment before checking your Sporran wallet and PayPal
                 balance. After you have confirmed that your account does not
-                have a DID and your payment method was not charged, please check
-                your internet connection and try again.
+                have a {web3name ? 'web3name' : 'DID'} and your payment method
+                was not charged, please check your internet connection and try
+                again.
               </p>
             </Fragment>
           )}
